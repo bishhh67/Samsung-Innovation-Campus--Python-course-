618bab8d9a485fffa6f264f403e180fb48a0802e
Bishleshan Paudel <081bct030.bishleshan@pcampus.edu.np>
2026-01-29 08:24:48 +0545

    from day 9 to day 16

---
+++ day17/calc.py
@@ -0,0 +1,21 @@
+from font import font1 as funn, font2
+
+# from font import * # everything
+
+
+def main():
+  print("inside main")
+  
+  funn()
+  font2()
+
+main()
+
+
+# if we just wrote :
+# import font   ( file)
+
+# we use dot operator to access
+
+# font.font1()
+# font.font2()
---
+++ day17/font.py
@@ -0,0 +1,5 @@
+def font1():
+  print("font is called")
+
+def font2():
+  print("inside font2")
